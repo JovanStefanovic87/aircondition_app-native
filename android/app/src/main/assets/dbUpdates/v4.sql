ab8699599699b0119958e702bc550599cda2c149
DESKTOP-4N736AQ\soxda <soxdarko@gmail.com>
2024-05-28 23:58:21 +0200

    Moving display order from DeviceStateComponent to ElementTitle. Adding more data for step 4.

--- android/app/src/main/assets/dbUpdates/v4.sql
+++ android/app/src/main/assets/dbUpdates/v4.sql
@@ -1,25 +1,35 @@
 -- update_v4.sql
 
-INSERT INTO GroupType (name) VALUES ('PHYSIKALISCH');
-INSERT INTO GroupType (name) VALUES ('KONSTRUKTIV');
-INSERT INTO GroupType (name) VALUES ('MIKROBIOLOGISCH');
-INSERT INTO GroupType (name) VALUES ('LUFTKEIMZAHLMESSUNG');
+-- INSERT INTO GroupType (name) VALUES ('PHYSIKALISCH');
+-- INSERT INTO GroupType (name) VALUES ('KONSTRUKTIV');
+-- INSERT INTO GroupType (name) VALUES ('MIKROBIOLOGISCH');
+-- INSERT INTO GroupType (name) VALUES ('LUFTKEIMZAHLMESSUNG');
 
 
-INSERT INTO TitleComponent (name, isUsingImage) VALUES ('ANLAGE', true);
-INSERT INTO TitleComponent (name, isUsingImage) VALUES ('ANLAGE', false);
-INSERT INTO TitleComponent (name, isUsingImage) VALUES ('GESAMT', true);
-INSERT INTO TitleComponent (name, isUsingImage) VALUES ('AUSSEN', true);
+-- INSERT INTO TitleComponent (id, name, isUsingImage) VALUES (1, 'ANLAGE', true);
+-- INSERT INTO TitleComponent (id, name, isUsingImage) VALUES (2, 'ANLAGE', false);
+-- INSERT INTO TitleComponent (id, name, isUsingImage) VALUES (3, 'GESAMT', true);
+-- INSERT INTO TitleComponent (id, name, isUsingImage) VALUES (4, 'AUSSEN', true);
+-- INSERT INTO TitleComponent (id, name, isUsingImage) VALUES (5, 'DÄMPFELEMENT', true);
 
-INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote, displayOrder) VALUES (12, 'Schmutz', 1, 2, true, 1);
-INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote, displayOrder) VALUES (13, 'Korrosion', 1, 2, true, 2);
-INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote, displayOrder) VALUES (14, 'Blätter', 1, 2, false, 3);
-INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote, displayOrder) VALUES (15, 'Beschädigung', 2, 2, false, 4);
-INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote, displayOrder) VALUES (16, 'Nicht einsehbar', 2, 2, false, 5);
-INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote, displayOrder) VALUES (17, 'Schutzgitter nicht vorhanden', 2, 2, false, 6);
-INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote, displayOrder) VALUES (18, 'Abstand AL-Öffnung zu Aufbau nicht ausreichend', 2, 2, false, 7);
-INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote, displayOrder) VALUES (19, 'Abstand AL-Öffnung zu Boden nicht ausreichend', 2, 2, false, 8);
-INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote, displayOrder) VALUES (20, 'Abstand AL-Öffnung zu FL-AL nicht ausreichend', 2, 2, false, 9);
+-- INSERT INTO StateType (id, name) VALUES (1, 'State of whole device');
+-- INSERT INTO StateType (id, name) VALUES (2, 'State of device element');
+
+ä => Ä
+ö => Ö
+ü => Ü
+ß => SS
+
+INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote) VALUES (12, 'Schmutz', 1, 2, true);
+INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote) VALUES (13, 'Korrosion', 1, 2, true);
+INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote) VALUES (14, 'Blätter', 1, 2, false);
+INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote) VALUES (15, 'Beschädigung', 2, 2, false);
+INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote) VALUES (16, 'Nicht einsehbar', 2, 2, false);
+INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote) VALUES (17, 'Schutzgitter nicht vorhanden', 2, 2, false);
+INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote) VALUES (18, 'Abstand AL-Öffnung zu Aufbau nicht ausreichend', 2, 2, false);
+INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote) VALUES (19, 'Abstand AL-Öffnung zu Boden nicht ausreichend', 2, 2, false);
+INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote) VALUES (20, 'Abstand AL-Öffnung zu FL-AL nicht ausreichend', 2, 2, false);
+INSERT INTO DeviceStateComponent (id, name, groupTypeId, stateTypeId, isUsingNote) VALUES (21, 'Abstand AL-Öffnung zu FL-AL nicht ausreichend', 2, 2, false);
 
 
 INSERT INTO Device_StateValue (deviceStateComponentId, stateValueId) VALUES (12, 1);
@@ -44,12 +54,29 @@ INSERT INTO Device_StateValue (deviceStateComponentId, stateValueId) VALUES (20,
 INSERT INTO Device_StateValue (deviceStateComponentId, stateValueId) VALUES (20, 2);
 
 
-INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId) VALUES (12, 3, 1);
-INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId) VALUES (13, 3, 1);
-INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId) VALUES (14, 3, 1);
-INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId) VALUES (15, 3, 1);
-INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId) VALUES (16, 3, 1);
-INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId) VALUES (17, 3, 1);
-INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId) VALUES (18, 3, 1);
-INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId) VALUES (19, 3, 1);
-INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId) VALUES (20, 3, 1);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (12, 3, 1, 1);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (13, 3, 1, 2);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (14, 3, 1, 3);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (15, 3, 1, 4);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (16, 3, 1, 5);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (17, 3, 1, 6);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (18, 3, 1, 7);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (19, 3, 1, 8);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (20, 3, 1, 9);
+
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (12, 3, 2, 1);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (13, 3, 2, 2);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (16, 3, 2, 3);
+
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (12, 5, 3, 1);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (13, 5, 3, 2);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (15, 5, 3, 3);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (16, 5, 3, 4);
+
+
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (12, 3, 4, 1);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (13, 3, 4, 2);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (15, 3, 4, 3);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (16, 3, 4, 4);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (15, 3, 4, 5);
+INSERT INTO ElementAndTitle (deviceStateComponentId, titleComponentId, deviceElementId, displayOrder) VALUES (16, 3, 4, 6);
