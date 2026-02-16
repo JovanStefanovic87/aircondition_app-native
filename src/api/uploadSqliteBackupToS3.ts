36fdbe7a63a8e1d35eda986d0a09a3480d33606e
DESKTOP-4N736AQ\soxda <soxdarko@gmail.com>
2026-02-16 18:09:25 +0100

    Upgrade saving and getting database backup from private S3 bucket instead of public like before.

--- src/api/uploadSqliteBackupToS3.ts
+++ src/api/uploadSqliteBackupToS3.ts
@@ -65,14 +65,30 @@ const baseUrl = `https://${HETZNER_S3_ENDPOINT!.replace(
 )}/${HETZNER_BUCKET_NAME}`;
 
 export const backupDbExists = async (username: string) => {
-    const url = `${baseUrl}/ac/db/${username}/latest.db`;
-    const res = await fetch(url, { method: 'HEAD' });
+    const adminApiUrl = getAdminApiUrl();
+    const res = await fetch(`${adminApiUrl}/api/db-backup-download-url`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username }),
+    });
     return res.ok;
 };
 
 export const downloadLatestDb = async (username: string, targetPath: string) => {
-    const url = `${baseUrl}/ac/db/${username}/latest.db`;
+    const adminApiUrl = getAdminApiUrl();
     const tmpPath = `${RNFS.DocumentDirectoryPath}/latest.db`;
+
+    const res = await fetch(`${adminApiUrl}/api/db-backup-download-url`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username }),
+    });
+
+    if (!res.ok) {
+        throw new Error('Failed to get download URL');
+    }
+
+    const { url } = await res.json();
 
     await RNFS.downloadFile({
         fromUrl: url,
@@ -81,60 +97,3 @@ export const downloadLatestDb = async (username: string, targetPath: string) => 
 
     await RNFS.moveFile(tmpPath, targetPath);
 };
-
-// import RNFS from 'react-native-fs';
-// import { HETZNER_BUCKET_NAME, HETZNER_S3_ENDPOINT } from './helpers/constants';
-// import { getAdminApiUrl } from './helpers/functions';
-// import { findExistingDbPath, getDatabase } from '../../database/dbConnection/initDatabase';
-
-// export const uploadSqliteBackupToS3 = async (username: string) => {
-//     const adminApiUrl = getAdminApiUrl();
-
-//     const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
-//     const backupFileName = `backup-${timestamp}.db`;
-
-//     const dbPath = await findExistingDbPath();
-//     const tempBackupPath = `${RNFS.DocumentDirectoryPath}/${backupFileName}`;
-
-//     await getDatabase().executeSql('PRAGMA wal_checkpoint(FULL);');
-
-//     await RNFS.copyFile(dbPath, tempBackupPath);
-
-//     const response = await fetch(`${adminApiUrl}/api/db-backup-url`, {
-//         method: 'POST',
-//         headers: { 'Content-Type': 'application/json' },
-//         body: JSON.stringify({
-//             username,
-//             fileName: backupFileName,
-//         }),
-//     });
-
-//     if (!response.ok) {
-//         throw new Error(await response.text());
-//     }
-
-//     const { url, Key } = await response.json();
-
-//     const putRes = await fetch(url, {
-//         method: 'PUT',
-//         headers: {
-//             'Content-Type': 'application/octet-stream',
-//         },
-//         body: {
-//             uri: `file://${tempBackupPath}`,
-//             type: 'application/octet-stream',
-//             name: backupFileName,
-//         } as any,
-//     });
-
-//     if (!putRes.ok) {
-//         throw new Error('Database upload failed');
-//     }
-
-//     return {
-//         storagePathS3: `https://${HETZNER_S3_ENDPOINT?.replace(
-//             /^https?:\/\//,
-//             '',
-//         )}/${HETZNER_BUCKET_NAME}/${Key}`,
-//     };
-// };
